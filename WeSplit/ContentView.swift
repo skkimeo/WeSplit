4d9d2228e04124115be60a09b020c492d344f183
sun <skkimeo@gmail.com>
2021-09-20 20:10:36 +0900

    "basic functioning done"

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,9 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 0
+    @State private var tipPercentage = 0
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson : Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount * tipSelection / 100
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    
+                    Picker(selection: $numberOfPeople, label: Text("Number of People")) {
+                        ForEach(2 ..< 100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section(header: Text("How much tip do you want to leave?")) {
+                    Picker(selection: $tipPercentage, label: Text("Tip Percentage")) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section (header: Text("total for each person :")) {
+                    Text("$ \(totalPerPerson)")
+                }
+            }
+            .navigationBarTitle("WeSplit")
+        }
     }
 }
 
